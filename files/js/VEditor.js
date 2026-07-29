914458a8bff006b8408fcf41475a63b6616704af
Fabio Perboni <fabio@cedepp.it>
2026-07-29 10:17:13 +0200

    basic plugin configuration

--- files/js/VEditor.js
+++ files/js/VEditor.js
@@ -11,44 +11,27 @@ let tiny_css = (tinycfg.dark == 1 ? 'dark' : 'default');
 
 tinymce.init({
     selector: 'textarea',
-
     license_key: 'gpl',
-
     language: tinycfg.lang,
-
     menubar: tinycfg.menubar,
-
     plugins: tinycfg.plugins,
-
     toolbar: tinycfg.toolbar,
     toolbar_mode: tinycfg.toolbar_mode || 'sliding',
-
     height: parseInt(tinycfg.height || '600'),
-
-    resize: tinycfg.resize !== 'false',
-
+    resize: tinycfg.resize === 'true' || tinycfg.resize === '1',
     statusbar: tinycfg.statusbar !== 'false',
-
     branding: tinycfg.branding === 'true',
-
     promotion: tinycfg.promotion === 'true',
-
     browser_spellcheck: true,
-
     link_default_target: '_blank',
-
     paste_data_images: tinycfg.pasteimages === 'true',
     paste_as_text: tinycfg.pastetext === 'true',
-
     forced_root_block: 'div',
-
     force_br_newlines: false,
     convert_newlines_to_brs: false,
     remove_linebreaks: true,
-
     skin: tiny_skin,
     content_css: tiny_css,
-
     content_style: `
         body {
             font-family: "Segoe UI", Arial, sans-serif;
@@ -67,7 +50,6 @@ tinymce.init({
             padding: 4px;
         }
     `,
-
     setup: function (editor) {
 
         editor.on('change', function () {
